b63fc27c2cbed04ee4dfc1c63ef508d6c7de858f
Akil Burgess <akilburgess@gmail.com>
2012-01-16 22:47:15 +0000

    Renamed SL4 and WindowsPhone projects appropriately

--- src/NGitHub.Extras.SL4/Properties/AssemblyInfo.cs
+++ src/NGitHub.Extras.SL4/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NGitHub.WindowsPhone.Extras")]
+[assembly: AssemblyTitle("NGitHub.Extras.SL4")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -14,4 +13,4 @@ using System.Resources;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("95ae80d4-90de-4bc7-834a-c20633447291")]
+[assembly: Guid("937d9ac6-450e-4492-a5f9-c1707109ba06")]
