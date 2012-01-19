13a65a82dfbdc9d763f903439fb323817fac30f2
Akil Burgess <akilburgess@gmail.com>
2012-01-19 07:22:49 +0000

    Added WP7 Mango project

---
+++ src/NGitHub.Extras.WindowsPhone.Mango/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NGitHub.Extras.WindowsPhone")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8a022b88-7ef0-4288-943e-13c6bfd653a0")]
